713b9607a96398b5643fdd3eba6190671fead728
Abdelalim03 <ka_atoui@esi.dz>
2022-03-22 00:38:55 +0100

    first commit

---
+++ public/electron.js
@@ -0,0 +1,78 @@
+const path = require('path');
+
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+
+// Conditionally include the dev tools installer to load React Dev Tools
+let installExtension, REACT_DEVELOPER_TOOLS;
+
+if (isDev) {
+  const devTools = require("electron-devtools-installer");
+  installExtension = devTools.default;
+  REACT_DEVELOPER_TOOLS = devTools.REACT_DEVELOPER_TOOLS;
+}
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling
+// if (require("electron-squirrel-startup")) {
+//   app.quit();
+// }
+
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show:false,
+    icon:path.join(__dirname,"icon.png"),
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // and load the index.html of the app.
+  // win.loadFile("index.html");
+  win.once('ready-to-show', () => {
+      win.maximize()
+    win.show()
+  })
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+  // Open the DevTools.
+//   if (isDev) {
+//     win.webContents.openDevTools({ mode: 'detach' });
+//   }
+}
+
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow();
+
+  if (isDev) {
+    installExtension(REACT_DEVELOPER_TOOLS)
+      .then(name => console.log(`Added Extension:  ${name}`))
+      .catch(error => console.log(`An error occurred: , ${error}`));
+  }
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
