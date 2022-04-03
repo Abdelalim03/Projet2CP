1400dc12371ae59db90387fdeeb495f96bbb99dd
HimeurWail <kw_himeur@esi.dz>
2022-04-03 12:30:56 +0100

    Landing page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import EtesVous from "./pages/EtesVous/EtesVous";
 import Nom from "./pages/Nom/Nom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NoPage from "./pages/NoPage/NoPage";
+import Landing from "./pages/Landing/Landing";
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" >
-          <Route index element={<EtesVous />} />
+          <Route index element={<Landing />} />
           <Route path="nom" element={<Nom />} />
           <Route path="*" element={<NoPage />} />
         </Route>
