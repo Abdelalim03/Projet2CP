e8447ecc676d6f3a02ec5c09d57e2c482f427d78
Rayane Djouah <kr_djouah@esi.dz>
2022-04-03 23:29:42 +0100

    fix etes vous feature

--- src/pages/EtesVous/Card.js
+++ src/pages/EtesVous/Card.js
@@ -3,14 +3,14 @@ import React from "react";
 export default function Card({ path, scale, name }) {
   return (
     <div class="transform hover:scale-105 transition ease-out cursor-pointer">
-    <div class="inline-flex h-64 w-48 lg:h-72 lg:w-52">
+      <div class="inline-flex h-64 w-48 lg:h-72 lg:w-52">
         <div class="flex flex-1 h-full -rotate-3 transform rounded-2xl bg-[#C7DBFE] hover:bg-[#7EABFF] p-5">
           <div class="h-full flex-1 rotate-3 rounded-2xl border-2 border-[#194084] bg-[#FFC5C1]">
-              <img
-              className={"w-24 lg:w-32 absolute bottom-10 left-3 "+(scale)}
+            <img
+              className={"w-24 lg:w-32 absolute bottom-10 left-3 " + scale}
               src={path}
               alt="CardImage"
-              />
+            />
           </div>
         </div>
       </div>
--- src/pages/EtesVous/EtesVous.js
+++ src/pages/EtesVous/EtesVous.js
@@ -11,10 +11,14 @@ function EtesVous() {
       <h1 className="main-title">Êtes vous un</h1>
       <div class="flex mt-3 gap-32 justify-center">
         <Link to="/nom">
-            <Card path="/etesvous/Eleve.png" scale="scale-100" name="Elève" />
+          <Card path="/etesvous/Eleve.png" scale="scale-100" name="Elève" />
         </Link>
         <Link to="/enseignant">
-            <Card path="/etesvous/Enseignant.png" scale="scale-125" name="Enseignant" />
+          <Card
+            path="/etesvous/Enseignant.png"
+            scale="scale-125"
+            name="Enseignant"
+          />
         </Link>
       </div>
     </div>
