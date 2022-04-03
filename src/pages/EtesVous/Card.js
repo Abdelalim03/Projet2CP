1ec5211a99ea4accca5c429f5228273ae6bf5890
Rayane Djouah <kr_djouah@esi.dz>
2022-04-03 16:59:03 +0100

    fix feature

--- src/pages/EtesVous/Card.js
+++ src/pages/EtesVous/Card.js
@@ -1,10 +1,21 @@
 import React from "react";
 
-export default function Card() {
+export default function Card({ path, scale, name }) {
   return (
-    <div class="inline-flex h-64 w-48">
-      <div class="flex h-full flex-1 -rotate-3 transform rounded-2xl bg-[#C7DBFE] p-5">
-        <div class="h-full flex-1 rotate-3 rounded-2xl border-2 border-[#194084] bg-[#FFC5C1]" />
+    <div class="transform hover:scale-105 transition ease-out cursor-pointer">
+    <div class="inline-flex h-64 w-48 lg:h-72 lg:w-52">
+        <div class="flex flex-1 h-full -rotate-3 transform rounded-2xl bg-[#C7DBFE] hover:bg-[#7EABFF] p-5">
+          <div class="h-full flex-1 rotate-3 rounded-2xl border-2 border-[#194084] bg-[#FFC5C1]">
+              <img
+              className="w-24 lg:w-32 absolute bottom-6 left-6"
+              src={path}
+              alt="CardImage"
+              />
+          </div>
+        </div>
+      </div>
+      <div class="mt-3 text-center font-semibold text-xl">
+        <span>{name}</span>
       </div>
     </div>
   );
--- src/pages/EtesVous/EtesVous.js
+++ src/pages/EtesVous/EtesVous.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import Absolute from "./Absolute";
 import Card from "./Card";
@@ -10,32 +9,12 @@ function EtesVous() {
     <div className="relative w-screen h-screen ">
       <Absolute />
       <h1 className="main-title">Êtes vous un</h1>
-      <div class="flex absolute ml-[35%] mb-[24%]">
+      <div class="flex gap-32 justify-center">
         <Link to="/nom">
-          <div class="relative hover:shadow-md transform hover:scale-105 transition ease-out cursor-pointer">
-            <Card />
-            <img
-              className="w-28 inline-block absolute bottom-16 left-10"
-              src="/etesvous/Eleve.png"
-              alt="Eleve"
-            />
-            <div class="mt-3 text-center font-semibold text-xl">
-              <span>Elève</span>
-            </div>
-          </div>
+            <Card path="/etesvous/Eleve.png" name="Elève" />
         </Link>
         <Link to="/enseignant">
-          <div class="relative ml-32 hover:shadow-md transform hover:scale-105 transition ease-out cursor-pointer">
-            <Card />
-            <img
-              className="w-36 inline-block absolute bottom-14 left-5"
-              src="/etesvous/Enseignant.png"
-              alt="Enseignant"
-            />
-            <div class=" mt-3 text-center font-semibold text-xl">
-              <span>Enseignant</span>
-            </div>
-          </div>
+            <Card path="/etesvous/Enseignant.png" name="Enseignant" />
         </Link>
       </div>
     </div>
