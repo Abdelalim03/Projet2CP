5bc09857c99699a0fe23e087988a6c33fabbc195
Rayane Djouah <kr_djouah@esi.dz>
2022-04-03 17:20:28 +0100

    fix feature

--- src/pages/EtesVous/Card.js
+++ src/pages/EtesVous/Card.js
@@ -7,7 +7,7 @@ export default function Card({ path, scale, name }) {
         <div class="flex flex-1 h-full -rotate-3 transform rounded-2xl bg-[#C7DBFE] hover:bg-[#7EABFF] p-5">
           <div class="h-full flex-1 rotate-3 rounded-2xl border-2 border-[#194084] bg-[#FFC5C1]">
               <img
-              className="w-24 lg:w-32 absolute bottom-6 left-6"
+              className="w-24 lg:w-32 {scale} absolute bottom-6 left-6"
               src={path}
               alt="CardImage"
               />
--- src/pages/EtesVous/EtesVous.js
+++ src/pages/EtesVous/EtesVous.js
@@ -11,10 +11,10 @@ function EtesVous() {
       <h1 className="main-title">Êtes vous un</h1>
       <div class="flex gap-32 justify-center">
         <Link to="/nom">
-            <Card path="/etesvous/Eleve.png" name="Elève" />
+            <Card path="/etesvous/Eleve.png" scale="scale-100" name="Elève" />
         </Link>
         <Link to="/enseignant">
-            <Card path="/etesvous/Enseignant.png" name="Enseignant" />
+            <Card path="/etesvous/Enseignant.png" scale="scale-125" name="Enseignant" />
         </Link>
       </div>
     </div>
