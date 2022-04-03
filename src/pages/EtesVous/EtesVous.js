13515a78855dae95ee2f33ac21abd66e79751720
Rayane Djouah <kr_djouah@esi.dz>
2022-04-03 14:36:15 +0100

    rayane/feature/etesvous

--- src/pages/EtesVous/EtesVous.js
+++ src/pages/EtesVous/EtesVous.js
@@ -13,7 +13,7 @@ function EtesVous() {
 
       <div class="flex absolute ml-[35%] mb-[24%]">
         <Link to="/nom">
-          <div class="relative hover:shadow-lg transform hover:scale-110 transition ease-out cursor-pointer">
+          <div class="relative hover:shadow-lg transform hover:scale-105 transition ease-out cursor-pointer">
             <div class="inline-flex h-64 w-48 items-center justify-center">
               <div class="flex h-full flex-1 -rotate-3 transform items-center justify-center rounded-2xl bg-[#C7DBFE] p-5">
                 <div class="h-full flex-1 rotate-3 rounded-2xl border-2 border-[#194084] bg-[#FFC5C1]" />
@@ -30,7 +30,7 @@ function EtesVous() {
           </div>
         </Link>
         <Link to="/enseignant">
-          <div class="relative ml-28 hover:shadow-lg transform hover:scale-105 transition ease-out cursor-pointer">
+          <div class="relative ml-32 hover:shadow-lg transform hover:scale-105 transition ease-out cursor-pointer">
           <div class="inline-flex h-64 w-48 items-center justify-center">
               <div class="flex h-full flex-1 -rotate-3 transform items-center justify-center rounded-2xl bg-[#C7DBFE] p-5">
                 <div class="h-full flex-1 rotate-3 rounded-2xl border-2 border-[#194084] bg-[#FFC5C1]" />
