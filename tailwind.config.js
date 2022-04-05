d0599d6102cb1c90ef5d99bc552fccb5cd95ee0d
Amine <ka_adjou@esi.dz>
2022-04-05 10:07:59 +0100

    Menu 85%

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors : { 
-        "symapp-blue-light":"#C7DBFE",
+      colors: {
+        "symapp-blue-light": "#C7DBFE",
+        "symapp-purple": "#6A5CF7",
+        "symapp-orange": "#FF946B",
+        "symapp-blue-light-second": "#98BCFF",
+        "symapp-pink-light": "#FDE3DE",
       },
     },
   },
   plugins: [],
-}
+};
