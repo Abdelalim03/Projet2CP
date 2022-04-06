cd171d3f34e1ab18e29794d3310e5e5a1799c48e
Rayane Djouah <kr_djouah@esi.dz>
2022-04-06 22:30:24 +0100

    quiz page added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Langue from "./pages/Langue/Langue";
 import NoPage from "./pages/NoPage/NoPage";
-import Landing from "./pages/Landing/Landing";
-import Menu from "./pages/Menu/Menu";
-import Avatar from "./pages/Avatar/Avatar";
-import EtesVous from "./pages/EtesVous/EtesVous";
-import Nom from "./pages/Nom/Nom";
+import Quiz from "./pages/Quiz/Quiz";
 
 function App() {
   return (
       <Router>
       <Routes>
         <Route path="/" >
-          <Route index element={<Langue />} />
-          <Route path="welcome"  >
-            <Route path="arabe" element={<NoPage />} />
-            <Route path="francais" element={<Landing />} />
-          </Route>
-          <Route path="choix" element={<EtesVous />} />
-          <Route path="nom" element={<Nom />} />
-          <Route path="avatar" element={<Avatar />} />
-          <Route path="menu" element={<Menu />}>
-          {/* <Route path=":avatarId" element={<Menu />} /> */}
-          </Route>
+          <Route index element={<Quiz />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
