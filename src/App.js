7ce72fccede503cdf7c0bef69569ca9a55f08425
HimeurWail <kw_himeur@esi.dz>
2022-05-01 00:42:24 +0100

    Editor page added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import Langue from "./pages/Langue/Langue";
 import Quiz from "./pages/Quiz/Quiz";
 import Layout from "./Components/Layout";
 import CoursContent from "./pages/Cours/CoursContent";
+import Exercices2 from "./pages/Exercices/Exercices2";
+import BoxBloquee from "./pages/Exercices/BoxBloquee";
 
 function App() {
   return (
