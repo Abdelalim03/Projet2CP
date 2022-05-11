29ff1a7e95dfcc9e009382a32319e7afaaa584e7
Abdelalim03 <ka_atoui@esi.dz>
2022-05-11 21:58:39 +0100

    some changes in editor

--- src/pages/Editeur/EditeurFr.js
+++ src/pages/Editeur/EditeurFr.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import SideBar from '../../Components/SideBar'
-import UserTitle from '../../Components/UserTitle'
 import InsPolygoneContent from './InsPolygoneContent'
 import RotateContent from './RotateContent'
 function EditeurFr() {
