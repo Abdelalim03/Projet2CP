821fa5bd05055e2e8fbc13ea872f2a853beb1e3e
Amine <ka_adjou@esi.dz>
2022-05-21 23:58:46 +0100

    Version form Complete +  t3mar DB

--- src/pages/Cours/Form.js
+++ src/pages/Cours/Form.js
@@ -30,6 +30,8 @@ function Form(props) {
       "titre": inputs.titreCours,
       "titreAr": inputs.titreCoursAr,
       "QuizExp": 20,
+      "QuestQuizFr": inputs.QuestQuizFr,
+      "QuestQuizAr": inputs.QuestQuizAr,
       "Prop1": inputs.prop1,
       "Prop1Ar": inputs.prop1Ar,
       "Prop2": inputs.prop2,
@@ -164,6 +166,30 @@ function Form(props) {
           
           <div className=' h-[130%] w-2/3 pl-4 mb-8 flex flex-col bg-[#90E0EF]/40 justify-around ml-32 rounded-xl'>
           <label className='block ml-5 text-sm lg:text-lg font-semibold'> Veuillez Insérer les propositions du quiz </label>
+            <label for="QuestQuizFr" className='block ml-5 text-sm lg:text-lg font-semibold'>La question du quiz : </label>
+            <input 
+                    onChange={handleChange}
+                    value={inputs.QuestQuizFr || ""}
+                    className='block border-2 rounded-[5px] text-sm lg:text-lg w-[300px] lg:w-[600px] pl-3'
+                    type="text" id="QuestQuizFr" name="QuestQuizFr"
+                    required
+                    placeholder="Entrez la question du quiz  "
+                    onInput={handleInput}
+                    onInvalid={handleInvalide}
+                    pattern="\S+"
+            />
+            <label for="QuestQuizAr" className='block ml-5 text-sm lg:text-lg font-semibold'>La question du quiz en arabe : </label>
+            <input 
+                    onChange={handleChange}
+                    value={inputs.QuestQuizAr || ""}
+                    className='block border-2 rounded-[5px] text-sm lg:text-lg w-[300px] lg:w-[600px] pl-3'
+                    type="text" id="QuestQuizAr" name="QuestQuizAr"
+                    required
+                    placeholder="Rentrez la question du quiz ( arabe )  "
+                    onInput={handleInput}
+                    onInvalid={handleInvalide}
+                    pattern="\S+"
+            />
             <label for="Proposition1" className='block ml-5 text-sm lg:text-lg font-semibold'>Première proposition : </label>
             <input 
                     onChange={handleChange}
